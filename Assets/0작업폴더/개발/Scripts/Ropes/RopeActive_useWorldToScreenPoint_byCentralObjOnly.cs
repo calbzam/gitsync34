af0ec57ad314c1b76baac9e267acc847a7a773d5
nebobyeoli@gmail.com <>
2024-07-20 17:57:27 +0200

    OnBecameVisible을 이용하여, 카메라 범위를 벗어나는 위치의 ObiRope Solver들 해제시킴 테스트 (각 rope 오브젝트의 스크립트에서 조정 방식)

--- Assets/0작업폴더/개발/Scripts/Ropes/RopeActive_useWorldToScreenPoint_byCentralObjOnly.cs
+++ Assets/0작업폴더/개발/Scripts/Ropes/RopeActive_useWorldToScreenPoint_byCentralObjOnly.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using Obi;
 
-public class RopeVisibilityController_useWorldToScreenPoint_byCentralObjOnly : MonoBehaviour
+public class RopeActive_useWorldToScreenPoint_byCentralObjOnly : MonoBehaviour
 {
     [SerializeField] private ObiUpdater _updater;
     private ObiSolver[] _solvers;
