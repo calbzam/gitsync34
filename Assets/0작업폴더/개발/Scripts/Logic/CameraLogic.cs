f807bb6f3178048bd48dc44bf307fc5a4e5c8aa4
nebobyeoli@gmail.com <>
2024-08-06 08:47:48 +0200

    카메라 전환 버튼 추가

---
+++ Assets/0작업폴더/개발/Scripts/Logic/CameraLogic.cs
@@ -0,0 +1,36 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraLogic : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera _defaultVirtualCam;
+    [SerializeField] private CinemachineVirtualCamera _freeVirtualCam;
+    [SerializeField] private TMP_Text _toPlayerCamText;
+    [SerializeField] private TMP_Text _toFreeCamText;
+
+    private bool _isFreeCam = false;
+
+    private void Start()
+    {
+        _defaultVirtualCam.gameObject.SetActive(true);
+
+        SetFreeCam(_isFreeCam = false);
+    }
+
+    public void ToggleFreeCam()
+    {
+        SetFreeCam(_isFreeCam = !_isFreeCam);
+    }
+
+    private void SetFreeCam(bool freeCam)
+    {
+        if (freeCam) _freeVirtualCam.transform.position = new Vector3(_defaultVirtualCam.transform.position.x, _defaultVirtualCam.transform.position.y, _freeVirtualCam.transform.position.z);
+
+        _freeVirtualCam.gameObject.SetActive(freeCam);
+        _toPlayerCamText.gameObject.SetActive(freeCam);
+        _toFreeCamText.gameObject.SetActive(!freeCam);
+    }
+}
