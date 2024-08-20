e27a3fd4a10e7d29f2d84c626fcf32e7d5d9135e
nebobyeoli@gmail.com <>
2024-08-20 20:34:08 +0200

    테스트용 기능들 숨기기 보이기 버튼 추가

---
+++ Assets/0작업폴더/개발/Scripts/ShowHideToolButtons.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowHideToolButtons : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _hideToolsText;
+    [SerializeField] private TMP_Text _showToolsText;
+    [SerializeField] private Button[] _toolButtonsToHide;
+
+    private bool _toolsHidden = false;
+
+    public void ToggleToolsHidden()
+    {
+        SetToolsHidden(_toolsHidden = !_toolsHidden);
+    }
+
+    private void Start()
+    {
+        SetToolsHidden(_toolsHidden = false);
+    }
+
+    private void SetToolsHidden(bool hideTools)
+    {
+        foreach (Button button in _toolButtonsToHide)
+        {
+            button.gameObject.SetActive(!hideTools);
+        }
+        _showToolsText.gameObject.SetActive(hideTools);
+        _hideToolsText.gameObject.SetActive(!hideTools);
+    }
+}
