47737b131b093bb9bb97cc2cfa901db2b6d837d1
nebobyeoli@gmail.com <>
2024-09-07 14:13:35 +0200

    Stage #1 (0707) 배경음악 추가

---
+++ Assets/0작업폴더/개발/Scripts/Sounds/BGMPlayer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource _bgm;
+    
+    private void Update()
+    {
+        if (!_bgm.isPlaying)
+        {
+            _bgm.Play();
+        }
+    }
+}
